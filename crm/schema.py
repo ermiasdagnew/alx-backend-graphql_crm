29eb0e9f79d76f8d6db14073c6bd6f65411ba841
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-12-12 13:51:43 +0300

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -5,43 +5,49 @@ from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Customer
 
+
 class CustomerNode(DjangoObjectType):
-class Meta:
-model = Customer
-filter_fields = {
-"name": ["exact", "icontains", "istartswith"],
-"email": ["exact", "icontains"],
-}
-interfaces = (relay.Node,)
+    class Meta:
+        model = Customer
+        filter_fields = {
+            "name": ["exact", "icontains", "istartswith"],
+            "email": ["exact", "icontains"],
+        }
+        interfaces = (relay.Node,)
+
 
 class CustomerType(DjangoObjectType):
-class Meta:
-model = Customer
+    class Meta:
+        model = Customer
+
 
 class CreateCustomer(graphene.Mutation):
-class Arguments:
-name = graphene.String(required=True)
-email = graphene.String(required=True)
-phone = graphene.String(required=True)
+    class Arguments:
+        id = graphene.String(required=True)
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+        phone = graphene.String(required=True)
 
-```
-customer = graphene.Field(CustomerType)
+    customer = graphene.Field(CustomerType)
 
-def mutate(self, info, name, email, phone):
-    customer = Customer(
-        name=name,
-        email=email,
-        phone=phone,
-    )
-    customer.save()
-    return CreateCustomer(customer=customer)
-```
+    def mutate(self, info, id, name, email, phone):
+        customer = Customer(
+            id=id,
+            name=name,
+            email=email,
+            phone=phone,
+        )
+        customer.save()
+        return CreateCustomer(customer=customer)
+
 
 class Query(graphene.ObjectType):
-all_customers = DjangoFilterConnectionField(CustomerNode)
-customer = relay.Node.Field(CustomerNode)
+    all_customers = DjangoFilterConnectionField(CustomerNode)
+    customer = relay.Node.Field(CustomerNode)
+
 
 class Mutation(graphene.ObjectType):
-create_customer = CreateCustomer.Field()
+    create_customer = CreateCustomer.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
