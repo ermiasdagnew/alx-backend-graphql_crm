97da4a271b1921d943668292f3944016bed360fb
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-12-12 13:49:28 +0300

    Add customer creation mutation to schema

--- crm/schema.py
+++ crm/schema.py
@@ -14,8 +14,34 @@ filter_fields = {
 }
 interfaces = (relay.Node,)
 
+class CustomerType(DjangoObjectType):
+class Meta:
+model = Customer
+
+class CreateCustomer(graphene.Mutation):
+class Arguments:
+name = graphene.String(required=True)
+email = graphene.String(required=True)
+phone = graphene.String(required=True)
+
+```
+customer = graphene.Field(CustomerType)
+
+def mutate(self, info, name, email, phone):
+    customer = Customer(
+        name=name,
+        email=email,
+        phone=phone,
+    )
+    customer.save()
+    return CreateCustomer(customer=customer)
+```
+
 class Query(graphene.ObjectType):
 all_customers = DjangoFilterConnectionField(CustomerNode)
 customer = relay.Node.Field(CustomerNode)
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+create_customer = CreateCustomer.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
