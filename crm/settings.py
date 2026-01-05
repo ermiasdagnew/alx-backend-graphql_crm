db8901f05d87291e532fc6fa308d953331b9f264
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-05 12:10:57 +0300

    Configure Celery and update installed apps
    Added 'django_celery_beat' to installed apps and configured Celery settings.

--- crm/settings.py
+++ crm/settings.py
@@ -1,6 +1,23 @@
-from celery.schedules import crontab
+INSTALLED_APPS = [
+    # default Django apps...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
+    # your apps
+    'crm',
+
+    # Celery Beat
+    'django_celery_beat',
+]
+
+# Celery configuration
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+from celery.schedules import crontab
 
 CELERY_BEAT_SCHEDULE = {
     'generate-crm-report': {
